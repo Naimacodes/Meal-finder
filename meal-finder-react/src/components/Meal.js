6932509e4c2d355ec6a63e6df016341a49646100
Rory Gilmore <naima.adanahmed@gmail.com>
2020-03-04 21:17:22 +0100

    added single meal component

---
+++ meal-finder-react/src/components/Meal.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import MealfinderContext from '../context/meal-finder/mealfinderContext';
+
+const Meal = ({meal}) => {
+
+  const mealfinderContext = useContext(MealfinderContext);
+  
+  return (
+    <div class="single-meal">
+      <h1>${meal.strMeal}</h1>
+      <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+      <div class="single-meal-info">
+        ${meal.strCategory ? `<p>${meal.strCategory}</p>` : ''}
+        ${meal.strArea ? `<p>${meal.strArea}</p>` : ''}
+      </div>
+      <div class="main">
+        <p>${meal.strInstructions}</p>
+        <h2>Ingredients</h2>
+        <ul>
+          ${ingredients.map(ing => `<li>${ing}</li>`).join('')}
+        </ul>
+      </div>
+    </div>
+  
+  )
+}
+
+export default Meal
